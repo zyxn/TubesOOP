c7f0eb5f4dcbf83b44bf30ea6df9cccc7a340c09
Zado <zado.keren@gmail.com>
2023-12-26 12:56:24 +0700

    delete password

--- src/Model/api/Database.java
+++ src/Model/api/Database.java
@@ -14,53 +14,45 @@ package Model.api;
  * and open the template in the editor.
  */
 import java.sql.*;
-import javax.swing.*;
-
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
 import javax.swing.JOptionPane;
 
-
 public class Database {
+
     final String DB_URL = "jdbc:mysql://localhost/tubes";
     final String DB_USER = "root";
-    final String DB_PASS = "z4d0s44d1";
+    final String DB_PASS = "Pass";
     Connection conn;
     Statement stmt;
     ResultSet rs;
-    
-    
-    public Database() throws SQLException{
-        try{
-            conn = DriverManager.getConnection(DB_URL,DB_USER,DB_PASS);
+
+    public Database() throws SQLException {
+        try {
+            conn = DriverManager.getConnection(DB_URL, DB_USER, DB_PASS);
             stmt = conn.createStatement();
             System.out.println("Connected");
-        }catch(Exception e){
-            JOptionPane.showMessageDialog(null,""+e.getMessage(),"ConnectionError", JOptionPane.WARNING_MESSAGE);
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "" + e.getMessage(), "ConnectionError", JOptionPane.WARNING_MESSAGE);
             throw e;
         }
     }
-    public ResultSet getData(String SQLString){
-        try{
+
+    public ResultSet getData(String SQLString) {
+        try {
             rs = stmt.executeQuery(SQLString);
-        }catch(Exception e){
-            JOptionPane.showMessageDialog(null,"Error :"+e.getMessage(),"Communication Error",JOptionPane.WARNING_MESSAGE);
-        }return rs;
-    }
-    
-   
-    public void query (String SQLString){
-       try{
-        stmt.executeUpdate(SQLString);
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Error :" + e.getMessage(), "Communication Error", JOptionPane.WARNING_MESSAGE);
         }
-       catch(Exception e){
-           JOptionPane.showMessageDialog(null,"Error :"+e.getMessage(),"Communication Error", JOptionPane.WARNING_MESSAGE);
-       }
+        return rs;
     }
-    
+
+    public void query(String SQLString) {
+        try {
+            stmt.executeUpdate(SQLString);
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Error :" + e.getMessage(), "Communication Error", JOptionPane.WARNING_MESSAGE);
+        }
+    }
+
     public PreparedStatement prepareStatement(String SQLString, Object[] parameters) {
         try {
             PreparedStatement preparedStatement = conn.prepareStatement(SQLString);
@@ -69,11 +61,11 @@ public class Database {
             for (int i = 0; i < parameters.length; i++) {
                 preparedStatement.setObject(i + 1, parameters[i]);
             }
-            
+
             return preparedStatement;
         } catch (SQLException e) {
             JOptionPane.showMessageDialog(null, "Error: " + e.getMessage(), "PreparedStatement Error", JOptionPane.WARNING_MESSAGE);
             return null;
         }
-}
+    }
 }
